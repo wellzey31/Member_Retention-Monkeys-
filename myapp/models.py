f175b3fdac541a0b15390a196d263e5d9c84021d
IanMorriso <112591354+IanMorriso@users.noreply.github.com>
2023-01-07 15:17:00 -0700

    Nothing

--- myapp/models.py
+++ myapp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-def members(request):
+class User(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     user_id = models.IntegerField(max_length=255)
