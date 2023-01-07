020a193a697dc5f13afbd1bcb4dbb510fd565999
Wells <brett.wells@uleth.ca>
2023-01-07 12:35:04 -0700

    hello world is working

--- MemberRetention/settings.py
+++ MemberRetention/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'MemberRetention.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['myapp/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
