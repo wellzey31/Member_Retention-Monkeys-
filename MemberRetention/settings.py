25fc0a14b8da63564346c8850748afd50d64bed3
IanMorriso <112591354+IanMorriso@users.noreply.github.com>
2023-01-07 17:51:10 -0700

    Added login verification and redirection

--- MemberRetention/settings.py
+++ MemberRetention/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myapp',
 ]
 
 SITE_ID = 1
@@ -126,3 +127,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = "/"
--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    user_id = models.IntegerField()
+    email = models.CharField(max_length=255)
+    phone = models.IntegerField()
+    age = models.IntegerField()
+    occupation = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    joined_date = models.DateField()
+    
+    
